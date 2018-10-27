f2db740b577cf3f4ee30465bceb8f1c1bebb9bf8
Max Rosenberg <mjr70@duke.edu>
2018-10-26 22:20:24 -0400

    l

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -33,8 +33,7 @@ public class LinkStrand implements IDnaStrand{
 		return mySize;
 		//return myInfo.length();
 	}
-	//Potentially StringBuilder.
-	//Work on this later.
+	//Initializes each of the instance variables for a given linkstrand
 	@Override
 	public void initialize(String source) {
 		Node news = new Node(source);
@@ -60,6 +59,8 @@ public class LinkStrand implements IDnaStrand{
 	 * myLast=myLast.next
 	 */
 	@Override
+	//goes through the nodes and adds the characters to the linkstrand's info and the length of 
+	//the string to the size and returns the mutated linkstrand
 	public IDnaStrand append(String dna) {
 		// TODO Auto-generated method stub
 		Node node1 = new Node(dna);
@@ -76,36 +77,14 @@ public class LinkStrand implements IDnaStrand{
 		//returns current Link
 		return this;
 	}
-	// Recursion 
-	// Find another solution but this is one
+	//reverses the first string and creates a new linkstrand with the reversed string intializing it.
+	//then it goes through the other nodes and reverses it and adds them to the front of the 
+	//linkstrand making sure to update variables
 	@Override
 	public IDnaStrand reverse() {
-		// TODO Auto-generated method stub
-		/*
-		Node first = myFirst;
-		StringBuilder str1 = new StringBuilder(first.info);
-		str1 = str1.reverse();
-		String backwards = str1.toString();
-		LinkStrand dna = new LinkStrand(backwards); 
-		Node node1 = myFirst.next;
 		
-		while (node1 != null) {
-			//node1 = node1.next;
-			StringBuilder rev1 = new StringBuilder(node1.info);
-			rev1 = rev1.reverse();
-			backwards = rev1.toString();
-			Node node2 = new Node (backwards);
-			node2.next = dna.myFirst;
-			dna.myFirst = node2;
-			node1 = node1.next;
-			}
-			dna.mySize = mySize;
-			dna.myAppends = myAppends;
-			return dna;
-		*/
 		Node first = myFirst;
 		StringBuilder news = new StringBuilder(first.info);
-		//built in function for StringBuilder
 		//ask not what an object can do you for you
 		//but what an object can do for itself -JFK
 		news = news.reverse();
@@ -119,38 +98,20 @@ public class LinkStrand implements IDnaStrand{
 		node1 = node1.next;
 			
 		while(node1 != null) {
-			//node1 = node1.next;
+			
 			totchars.append(node1.info);
 			StringBuilder news1 = new StringBuilder(node1.info);
 			news1 = news1.reverse();
 			Node node2 = new Node(news1.toString());
-			//int length = news1.toString().length();
 			
-			//System.out.println(dna.mySize);
 			dna.myLast = node1.next;
 			node2.next = dna.myFirst;
 			dna.myFirst = node2;
-			//dna.mySize += length;
 			node1 = node1.next;
 		}
 		//}
 		dna.mySize = totchars.toString().length();
 		return dna;
-		/*
-		LinkStrand dna = new LinkStrand();
-		Node first = myFirst;
-		Node node1 = null;
-		while (first != null) {
-			StringBuilder s1 = new StringBuilder(first.info);
-			s1 = s1.reverse();
-			Node node2 = new Node(s1.toString());
-			node2.next = node1;
-			node1.next = node2;
-			node2 = dna.myLast;
-			first = first.next;
-		}
-		return dna;
-		*/
 		
 	}
 
@@ -159,7 +120,9 @@ public class LinkStrand implements IDnaStrand{
 		// TODO Auto-generated method stub
 		return myAppends;
 	}
-
+	//checks tio make sure the index is within the bounds and then increments the local index and 
+	//index by one until the index is found or the local index exceeds the node's length, in which
+	//case the local index resets
 	@Override
 	public char charAt(int index) {
 		if (myIndex > index) {
@@ -189,8 +152,7 @@ public class LinkStrand implements IDnaStrand{
 		return myCurrent.info.charAt(myLocalIndex);
 	}
 		
-	//This some validd ass toString
-	//Might have some error
+	//takes the node and makes it into a string using a string builder to append
 	public String toString() {
 		StringBuilder info = new StringBuilder();
 		Node current = myFirst;
